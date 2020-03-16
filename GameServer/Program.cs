9d1d4ec243d2ee047e2424c0aa4dd647ad4e88bc
BoenYang <yang_7274@126.com>
2020-03-16 23:27:31 +0800

    kcp server

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -1,11 +1,37 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 
 namespace GameServer
 {
     class Program
     {
+        private KCPServer m_kcp;
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+           Program p = new Program();
+           p.Init();
+           p.Run();
+        }
+
+        public void Init()
+        {
+            m_kcp = new KCPServer(1001, 1);
+            m_kcp.AddReceiveListener(this.OnRecivedData);
+        }
+
+        private void OnRecivedData(byte[] data, int size)
+        {
+
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                m_kcp.Update();
+                Thread.Sleep(1);
+            }
         }
     }
 }
--- MyNetworkDll/Server/KCPServer.cs
+++ MyNetworkDll/Server/KCPServer.cs
@@ -14,9 +14,7 @@ public class KCPServer
         return (UInt32)(Convert.ToInt64(DateTime.UtcNow.Subtract(UTCTimeBegin).TotalMilliseconds) & 0xffffffff);
     }
 
-    public delegate void KCPReceiveListener(byte[] buff, int size, IPEndPoint remotePoint);
-
-
+    public delegate void KCPReceiveListener(byte[] buff, int size);
 
     public string LOG_TAG = "KCPSocket";
 
@@ -29,29 +27,23 @@ public class KCPServer
 
     //KCP参数
     private List<KCPProxy> m_ListKcp;
-    private uint m_KcpKey = 0;
     private KCPReceiveListener m_AnyEPListener;
 
     //=================================================================================
     #region 构造和析构
 
-    public KCPServer(int bindPort, uint kcpKey, AddressFamily family = AddressFamily.InterNetwork) {
+    public KCPServer(int bindPort, AddressFamily family = AddressFamily.InterNetwork) {
 
         m_AddrFamily = family;
-        m_KcpKey = kcpKey;
         m_ListKcp = new List<KCPProxy>();
 
         m_SystemSocket = new Socket(m_AddrFamily, SocketType.Dgram, ProtocolType.Udp);
         IPEndPoint ipep = new IPEndPoint(IPAddress.Any, bindPort);
         m_SystemSocket.Bind(ipep);
 
-        bindPort = (m_SystemSocket.LocalEndPoint as IPEndPoint).Port;
-        LOG_TAG = "KCPSocket[" + bindPort + "-" + kcpKey + "]";
-
         m_IsRunning = true;
         m_ThreadRecv = new Thread(Thread_Recv) { IsBackground = true };
         m_ThreadRecv.Start();
-
 
     }
 
@@ -119,25 +111,24 @@ public class KCPServer
         return true;
     }
 
-    private KCPProxy GetKcp(IPEndPoint ipep, bool autoAdd = true) {
-        if (!IsValidKcpIPEndPoint(ipep)) {
-            return null;
-        }
+    private KCPProxy GetKcp(uint sid) {
 
         KCPProxy proxy = null;
         int cnt = m_ListKcp.Count;
         for (int i = 0; i < cnt; i++) {
             proxy = m_ListKcp[i];
-            if (proxy.RemotePoint.Equals(ipep)) {
+            if (proxy.sid == sid) {
                 return proxy;
             }
         }
+        return proxy;
+    }
 
-        if (autoAdd) {
-            proxy = new KCPProxy(m_KcpKey, ipep, m_SystemSocket);
-            proxy.AddReceiveListener(OnReceiveAny);
-            m_ListKcp.Add(proxy);
-        }
+    private KCPProxy CreateKcp(uint sid, IPEndPoint ipep)
+    {
+        KCPProxy proxy = new KCPProxy(sid, ipep, m_SystemSocket);
+        proxy.AddReceiveListener(OnReceiveAny);
+        m_ListKcp.Add(proxy);
         return proxy;
     }
 
@@ -155,31 +146,6 @@ public class KCPServer
     }
 
     #endregion
-
-    //=================================================================================
-    #region 发送逻辑
-    public bool SendTo(byte[] buffer, int size, IPEndPoint remotePoint) {
-        if (remotePoint.Address == IPAddress.Broadcast) {
-            int cnt = m_SystemSocket.SendTo(buffer, size, SocketFlags.None, remotePoint);
-            return cnt > 0;
-        }
-        else {
-            KCPProxy proxy = GetKcp(remotePoint);
-            if (proxy != null) {
-                return proxy.DoSend(buffer, size);
-            }
-        }
-
-        return false;
-    }
-
-    public bool SendTo(string message, IPEndPoint remotePoint) {
-        byte[] buffer = Encoding.UTF8.GetBytes(message);
-        return SendTo(buffer, buffer.Length, remotePoint);
-    }
-
-    #endregion
-
 
     //=================================================================================
     #region 主线程驱动
@@ -202,26 +168,6 @@ public class KCPServer
     //=================================================================================
     #region 接收逻辑
 
-    public void AddReceiveListener(IPEndPoint remotePoint, KCPReceiveListener listener) {
-        KCPProxy proxy = GetKcp(remotePoint);
-        if (proxy != null) {
-            proxy.AddReceiveListener(listener);
-        }
-        else {
-            m_AnyEPListener += listener;
-        }
-    }
-
-    public void RemoveReceiveListener(IPEndPoint remotePoint, KCPReceiveListener listener) {
-        KCPProxy proxy = GetKcp(remotePoint);
-        if (proxy != null) {
-            proxy.RemoveReceiveListener(listener);
-        }
-        else {
-            m_AnyEPListener -= listener;
-        }
-    }
-
     public void AddReceiveListener(KCPReceiveListener listener) {
         m_AnyEPListener += listener;
     }
@@ -231,9 +177,9 @@ public class KCPServer
     }
 
 
-    private void OnReceiveAny(byte[] buffer, int size, IPEndPoint remotePoint) {
+    private void OnReceiveAny(byte[] buffer, int size) {
         if (m_AnyEPListener != null) {
-            m_AnyEPListener(buffer, size, remotePoint);
+            m_AnyEPListener(buffer, size);
         }
     }
 
@@ -264,10 +210,17 @@ public class KCPServer
             SocketFlags.None, ref remotePoint);
 
         if (cnt > 0) {
-            KCPProxy proxy = GetKcp((IPEndPoint)remotePoint);
-            if (proxy != null) {
-                proxy.DoReceiveInThread(m_RecvBufferTemp, cnt);
+            byte[] m_32b = new byte[4];
+            Buffer.BlockCopy(m_RecvBufferTemp, 0, m_32b, 0, 4);
+            uint sid = BitConverter.ToUInt32(m_32b, 0);
+
+            KCPProxy proxy = GetKcp(sid);
+            if (proxy == null)
+            {
+                proxy = CreateKcp(sid, (IPEndPoint)remotePoint);
             }
+
+            proxy.DoReceiveInThread(m_RecvBufferTemp, cnt);
         }
 
     }
@@ -279,4 +232,136 @@ public class KCPServer
     }
 
     #endregion
+}
+
+internal class NetBufferReader
+{
+}
+
+
+class KCPProxy
+{
+
+    private KCP m_Kcp;
+    private bool m_NeedKcpUpdateFlag = false;
+    private uint m_NextKcpUpdateTime = 0;
+    private SwitchQueue<byte[]> m_RecvQueue = new SwitchQueue<byte[]>(128);
+
+    private IPEndPoint m_RemotePoint;
+    private Socket m_Socket;
+    private KCPServer.KCPReceiveListener m_Listener;
+
+    public IPEndPoint RemotePoint { get { return m_RemotePoint; } }
+
+    private uint m_sid;
+
+    public uint sid
+    {
+        get => m_sid;
+    }
+
+    public KCPProxy(uint key, IPEndPoint remotePoint, Socket socket) {
+        m_Socket = socket;
+        m_RemotePoint = remotePoint;
+
+        m_sid = key;
+
+        m_Kcp = new KCP(m_sid, HandleKcpSend);
+        m_Kcp.NoDelay(1, 10, 2, 1);
+        m_Kcp.WndSize(128, 128);
+
+    }
+
+    public void Dispose() {
+        m_Socket = null;
+
+        if (m_Kcp != null) {
+            m_Kcp.Dispose();
+            m_Kcp = null;
+        }
+
+        m_Listener = null;
+    }
+
+    //---------------------------------------------
+    private void HandleKcpSend(byte[] buff, int size) {
+        if (m_Socket != null) {
+            m_Socket.SendTo(buff, 0, size, SocketFlags.None, m_RemotePoint);
+        }
+    }
+
+    private void HandleKcpSend_Hook(byte[] buff, int size) {
+        if (m_Socket != null) {
+            m_Socket.SendTo(buff, 0, size, SocketFlags.None, m_RemotePoint);
+        }
+    }
+
+    public bool DoSend(byte[] buff, int size) {
+        m_NeedKcpUpdateFlag = true;
+        return m_Kcp.Send(buff, size) >= 0;
+    }
+
+    //---------------------------------------------
+
+    public void AddReceiveListener(KCPServer.KCPReceiveListener listener) {
+        m_Listener += listener;
+    }
+
+    public void RemoveReceiveListener(KCPServer.KCPReceiveListener listener) {
+        m_Listener -= listener;
+    }
+
+
+
+    public void DoReceiveInThread(byte[] buffer, int size) {
+
+
+        byte[] dst = new byte[size];
+        Buffer.BlockCopy(buffer, 0, dst, 0, size);
+        m_RecvQueue.Push(dst);
+    }
+
+    private void HandleRecvQueue() {
+        m_RecvQueue.Switch();
+        while (!m_RecvQueue.Empty()) {
+            var recvBufferRaw = m_RecvQueue.Pop();
+            int ret = m_Kcp.Input(recvBufferRaw);
+
+            //收到的不是一个正确的KCP包
+            if (ret < 0) {
+                if (m_Listener != null) {
+                    m_Listener(recvBufferRaw, recvBufferRaw.Length);
+                }
+                return;
+            }
+
+            m_NeedKcpUpdateFlag = true;
+            KCP kcp = m_Kcp;
+
+            for (int size = kcp.PeekSize(); size > 0; size = kcp.PeekSize()) {
+                var recvBuffer = new byte[size];
+                if (kcp.Recv(recvBuffer) > 0) {
+                    if (m_Listener != null) {
+                        m_Listener(recvBuffer, size);
+                    }
+                }
+            }
+        }
+    }
+
+    //---------------------------------------------
+    public void Update(uint currentTimeMS) {
+        HandleRecvQueue();
+
+        if (m_NeedKcpUpdateFlag || currentTimeMS >= m_NextKcpUpdateTime) {
+            if (m_Kcp != null) {
+                m_Kcp.Update(currentTimeMS);
+                m_NextKcpUpdateTime = m_Kcp.Check(currentTimeMS);
+                m_NeedKcpUpdateFlag = false;
+            }
+        }
+    }
+
+    //---------------------------------------------
+
 }
